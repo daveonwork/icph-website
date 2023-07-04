c81aff05312791b1157bb92f76afa4c947df6591
developertarlacmed <developer@tarlacmedicalcenter.com.ph>
2023-07-04 12:25:10 +0800

    Update Landing Doctors

--- resources/views/website/landing/doctors.blade.php
+++ resources/views/website/landing/doctors.blade.php
@@ -34,7 +34,7 @@
          
                   <div class="row justify-content-center">
                     <div class="col-md-8 text-center heading-section ftco-animate">
-                      <h2> <a href="/doctors" class="appointment-btn scrollto">View More Gallery<i class="bx bx-chevron-right"></i></a> </h2>
+                      <h2> <a href="/doctors" class="appointment-btn scrollto">View More Doctors<i class="bx bx-chevron-right"></i></a> </h2>
                    </div>
                 </div>	
               </div>                    
