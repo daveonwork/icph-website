e97cbb5ed7e144f396bcaaf679c2ef5051e22d7e
developertarlacmed <developer@tarlacmedicalcenter.com.ph>
2023-07-04 12:05:46 +0800

    Update Landing Doctors

--- resources/views/website/landing/doctors.blade.php
+++ resources/views/website/landing/doctors.blade.php
@@ -38,28 +38,5 @@
               </div>
             </div>
 
-            <div class="col-lg-6 mt-4">
-              <div class="member d-flex align-items-start">
-                <div class="pic"><img src="{{ asset ('img/doctors/doctors-def.jpg') }}" alt="" class="img-fluid"></div>
-                <div class="member-info">
-                  <h4>Amanda Jepson</h4>
-                  <span>Neurosurgeon</span>         
-                </div>
-              </div>
-            </div>
-
-          </div>
-
-
-          <section id="load" class="load">
-            <div class="container">
-       
-                <div class="row justify-content-center">
-                  <div class="col-md-8 text-center heading-section ftco-animate">
-                    <h2> <a href="/doctors" class="appointment-btn scrollto">View More Doctors<i class="bx bx-chevron-right"></i></a> </h2>
-                 </div>
-              </div>	
-            </div>                    
-           </section>
-        </div>
+                   </div>
       </section>
