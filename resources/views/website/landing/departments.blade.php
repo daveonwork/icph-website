dcaf71ab0336bc9ec36bd0dcb726ec38c12402f7
developertarlacmed <developer@tarlacmedicalcenter.com.ph>
2023-07-05 15:25:42 +0800

    Update Landing Department

--- resources/views/website/landing/departments.blade.php
+++ resources/views/website/landing/departments.blade.php
@@ -45,9 +45,7 @@
                 <div class="col-lg-8 details order-2 order-lg-1">
                 <h3>Cardiology</h3>
                 <p class="fst-italic">Our dedicated team of expert cardiologists, nurses, and healthcare professionals is here to provide you with top-notch care and support for all your cardiovascular needs. Whether you require a routine check-up, specialized diagnostic tests, or treatment options, we are committed to delivering exceptional cardiology services tailored to your individual needs.</p>
-                <p>Services Offered:</p>
-                <p>1. </p>
-                <p>2. </p>
+                
                 </div>
                 <div class="col-lg-4 text-center order-1 order-lg-2">
                 <div class="pic"><img src="{{ asset ('img/departments-1.jpg') }}" alt="" class="img-fluid"></div>
@@ -59,9 +57,7 @@
                 <div class="col-lg-8 details order-2 order-lg-1">
                 <h3>Internal Medicine</h3>
                 <p class="fst-italic">Our Internal Medicine Department is staffed by a team of highly skilled and compassionate physicians specializing in internal medicine. We are dedicated to providing exceptional medical care for adults of all ages, addressing a wide range of health concerns and promoting overall well-being.</p>
-                <p>Services Offered:</p>
-                <p>1. </p>
-                <p>2. </p>
+               
             </div>
                 <div class="col-lg-4 text-center order-1 order-lg-2">
                 <div class="pic"><img src="{{ asset ('img/internal-physician.jpeg') }}" alt="" class="img-fluid"></div>
@@ -73,9 +69,7 @@
                 <div class="col-lg-8 details order-2 order-lg-1">
                 <h3>Nephrology</h3>
                 <p class="fst-italic">At our Nephrology Department, your well-being is our top priority. We are committed to delivering high-quality, patient-centered care, focusing on improving kidney health, managing symptoms, and enhancing your overall quality of life.</p>
-                <p>Services Offered:</p>
-                <p>1. </p>
-                <p>2. </p>
+                
             </div>
                 <div class="col-lg-4 text-center order-1 order-lg-2">
                     <div class="pic"><img src="{{ asset ('img/nephrology.jpg') }}" alt="" class="img-fluid"></div>
@@ -87,9 +81,7 @@
                 <div class="col-lg-8 details order-2 order-lg-1">
                 <h3>Neuro-Psychiatry</h3>
                 <p class="fst-italic">We are dedicated to creating a supportive and compassionate environment where you can feel comfortable discussing your mental health and neurological concerns. Your care and privacy are of utmost importance to us, and we are committed to providing a safe and non-judgmental space for you to receive the help you need.</p>
-                <p>Services Offered:</p>
-                <p>1. </p>
-                <p>2. </p>
+                
             </div>
                 <div class="col-lg-4 text-center order-1 order-lg-2">
                     <div class="pic"><img src="{{ asset ('img/neuro.jpg') }}" alt="" class="img-fluid"></div>
@@ -101,9 +93,7 @@
                 <div class="col-lg-8 details order-2 order-lg-1">
                 <h3>Pediatrics</h3>
                 <p class="fst-italic">We offer a wide range of pediatric services to address the various aspects of your child's health. From routine check-ups and immunizations to the diagnosis and management of acute illnesses and chronic conditions, we are here to support your child's growth, development, and overall health.</p>
-                <p>Services Offered:</p>
-                <p>1. </p>
-                <p>2. </p>
+              
             </div>
                 <div class="col-lg-4 text-center order-1 order-lg-2">
                     <div class="pic"><img src="{{ asset ('img/pedia.jpg') }}" alt="" class="img-fluid"></div>
@@ -115,9 +105,7 @@
                     <div class="col-lg-8 details order-2 order-lg-1">
                     <h3>Obstetrics-Gynecology</h3>
                     <p class="fst-italic">Our services encompass routine gynecological exams, Pap smears, breast examinations, contraceptive counseling, and preconception care to help you achieve and maintain optimal reproductive health. We also provide comprehensive prenatal care for expectant mothers, guiding you through each stage of your pregnancy journey and supporting you in ensuring a healthy pregnancy and delivery.</p>
-                    <p>Services Offered:</p>
-                <p>1. </p>
-                <p>2. </p>
+                    
             </div>
                     <div class="col-lg-4 text-center order-1 order-lg-2">
                         <div class="pic"><img src="{{ asset ('img/obgyne.jpg') }}" alt="" class="img-fluid"></div>
@@ -129,10 +117,7 @@
                     <div class="col-lg-8 details order-2 order-lg-1">
                     <h3>Orthopedics</h3>
                     <p class="fst-italic">At our Orthopedics Department, we believe in a multidisciplinary approach to your care. Our team works closely with physical therapists, rehabilitation specialists, and pain management experts to provide comprehensive treatment plans that incorporate rehabilitation, physical therapy, and pain management techniques. Our goal is to optimize your recovery, restore function, and help you return to your normal activities as quickly and safely as possible.</p>
-                    <p>Services Offered:</p>
-                <p>1. </p>
-                <p>2. </p>
-                
+                                   
                 </div>
                     <div class="col-lg-4 text-center order-1 order-lg-2">
                         <div class="pic"><img src="{{ asset ('img/ortho.jpg') }}" alt="" class="img-fluid"></div>
@@ -155,9 +140,7 @@
                     <div class="col-lg-8 details order-2 order-lg-1">
                     <h3>Surgery</h3>
                     <p class="fst-italic">Our Surgery Department is staffed by a team of highly skilled surgeons, anesthesiologists, nurses, and support staff who are dedicated to delivering safe and effective surgical interventions across a wide range of specialties. We understand that undergoing surgery can be a significant event in your life, and we are committed to providing you with the highest level of care and support throughout your surgical journey.</p>
-                    <p>Services Offered:</p>
-                <p>1. </p>
-                <p>2. </p>
+                   
             </div>
                     <div class="col-lg-4 text-center order-1 order-lg-2">
                         <div class="pic"><img src="{{ asset ('img/surgery.jpg') }}" alt="" class="img-fluid"></div>
